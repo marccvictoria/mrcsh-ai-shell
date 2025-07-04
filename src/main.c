40addc3f81ab93abae353e6709e08d2036d53041
marccvictoria <marctristanvictoria@gmail.com>
2025-07-04 16:39:44 +0800

    updated comments

--- src/main.c
+++ src/main.c
@@ -35,10 +35,14 @@ void sh_ex()
     exit(EXIT_SUCCESS);
 }
 
-void sh_cd(char **us_direc_inp)
+// us_direc_inp is a pointer to an array of char *
+void sh_cd(char **us_direc_inp) // equiv to parsed_str, gets the second string after the command word
 {
     // remove surrounding quotes
-    char *path = us_direc_inp[1];
+    // path is now a pointer to the first character in the string ex: "HOME"
+    char *path = us_direc_inp[1]; // cd "PATH" wherein '"' is at [1]
+    // us_direc_inp[1] is of type char *, and we want path to point to the same string.
+    // So we declare path as *path
     if (path[0] == '"' && path[strlen(path) - 1] == '"')
     {
         path[strlen(path) - 1] = '\0'; // remove trailing quote
@@ -62,39 +66,55 @@ void sh_cd(char **us_direc_inp)
     }
 }
 
-char parse(char us_inp[64])
+char parse(char us_inp[BUFSIZ])
 {
-    char *parsed_str[64];
-    char *us_inp_m, *token;
-    int i = 0; // not defining this causes segfaults
+    char *parsed_str[BUFSIZ]; // parsed_str is an array of BUFSIZ pointers to char
+    char *us_inp_m, *token;   // var us_inp_m, token are pointers to char
+    int i = 0;                // not defining this causes segfaults
 
-    // make str modifiable
+    // make str modifiable by duplicating
     us_inp_m = strdup(us_inp);
 
     // split us_inp_m by spaces
     // execute while as long as it is not NULL
-    while ((token = strsep(&us_inp_m, " ")) != NULL) // // strsep slices between spaces: it moves to next delimeter, replaces with '\0' and returns the token
+    /*
+    char *strsep(char **stringp, const char *delim);
+    - returns the next token (as a char *) from the string you’re parsing i.e. us_inp[BUFSIZ].
+    - strsep() returns a pointer to the start of the current token, and modifies the original string by
+    inserting '\0' (null terminators) in place of delimiters.
+    */
+    while ((token = strsep(&us_inp_m, " ")) != NULL) // strsep slices between spaces: it moves to next delimeter, replaces with '\0' and returns the token
     {
-        if (*token == '\0') // there's nothing between a delimeter and a delimeter, returning empty string '\0'
-            continue;       // we skip these empty tokens
-        parsed_str[i++] = token;
+        // dereferencing: *token access value
+        if (*token == '\0')      // there's nothing between a delimeter and a delimeter, returning empty string '\0'
+            continue;            // we skip these empty tokens
+        parsed_str[i++] = token; // parsed_str[i] = token; i++;
     }
     // after the while loop termindated, i value is updated
 
-    char *builtin[] = {"cd", "exit", "echo", "help", "set", "unset"};
+    char *builtin[] = {"cd", "exit", "echo", "help", "set", "unset"}; // builtin array of each element in it is a pointer to char
+    // Each element of builtin[] is a char * — a pointer to the first character of a string.
     char *external[] = {};
     int size_builtin = sizeof(builtin) / sizeof(builtin[0]);
     int command_found = 0;
 
+    // checking if the command is available, if yes, classify and execute
     for (int k = 0; k < size_builtin; k++)
     {
         // print the parsed tokens
         for (int j = 0; j < i; j++)
         {
-            if (strcmp(parsed_str[j], builtin[k]) == 0) // find if command exits in builtin[], returns 0 if equal
+            // find if command exits in builtin[], returns 0 if equal
+            if (strcmp(parsed_str[j], builtin[k]) == 0)
             {
+                /*
+                Function Pointers
+                - func_ptr is an array of pointers to functions, where each function takes a char **
+                as an argument and returns void.
+                */
                 void (*func_ptr[])(char **) = {sh_cd, sh_ex, sh_echo};
-                func_ptr[k](parsed_str);
+                // k is index in function_ptr[] which matches position of keywords in builtin[]
+                func_ptr[k](parsed_str); // parsed_str: an array of char * decays into char **
                 command_found = 1;
             }
         }
@@ -111,20 +131,38 @@ char parse(char us_inp[64])
 int main(void)
 {
     int shell_on = 0;
-    char cwd[100];
-    char us_inp[64];
+    char cwd[100];       // buffer, array of characters where cwd path will be stored
+    char us_inp[BUFSIZ]; // input buffer, constant defined in <stdio.h>, has 512 bytes
 
-    curr_dir = getcwd(cwd, sizeof(cwd));
+    curr_dir = getcwd(cwd, sizeof(cwd)); // gets current working directory
+    /* If a function returns T *, you must assign it to a T * variable (or equivalent)
+     - getcwd() fills the buffer with path of cwd
+     - returns a pointer (char *) to the first character of the filled buffer (cwd[0]) if SUCCESSFUL;
+       NULL otherwise
+     - the pointer points to the filled buffer holding the current working directory
+
+     Strings in C
+     - Is just a pointer to the first character
+     - C can’t return entire arrays from functions — only pointers to them.
+     - C returns the memory address of where the string starts and tells us to go read it yourself
+     - ex: printf follows that pointer, reading one character at a time until it hits the '\0'.
+
+     - thus, curr_dir = cwd
+     - it takes a *buf, where the path will be stored
+     - and size of that buffer
+    */
 
     while (shell_on == 0)
     {
         printf("MRCSH-AI %s$ ", curr_dir);
+        // Format specifier %s in printf print characters at that address, not the address itself
+        // %p if address
 
         // read user input
-        fgets(us_inp, sizeof(us_inp), stdin);
-        us_inp[strcspn(us_inp, "\n")] = 0; // strcspn aka string complement span, searches for '\n' in us_inp, returns the index, and removes it by setting it to 0
+        fgets(us_inp, sizeof(us_inp), stdin); // fgets() read up to n - 1 characters from the given stream into the array str, and automatically append '\0' at the end.
+        us_inp[strcspn(us_inp, "\n")] = 0;    // strcspn aka string complement span, searches for '\n' in us_inp, returns the index, and removes it by setting it to 0
 
-        // parse
+        // parse and execute
         parse(us_inp);
     }
     return 0;
